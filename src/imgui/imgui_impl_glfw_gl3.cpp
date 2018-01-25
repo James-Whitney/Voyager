f5ef778dbfdb23a77eff5021b95b97596656a75e
Sean Slater <gits@spslater.com>
2018-01-24 21:22:55 -0800

    Use glad for FGLF calls

--- src/imgui/imgui_impl_glfw_gl3.cpp
+++ src/imgui/imgui_impl_glfw_gl3.cpp
@@ -13,7 +13,7 @@
 
 // GL3W/GLFW
 //#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
-#include "../GLSL.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #ifdef _WIN32
 #undef APIENTRY
