2cb1f9c6dcb6dc94b31d788c64ccba047994056f
Martynas Budvytis <martynasb96@gmail.com>
2018-02-15 21:20:07 -0800

    Generate terrain verticies, faces, and normals

--- src/voyager-utils/Terrain.cpp
+++ src/voyager-utils/Terrain.cpp
@@ -1,6 +1,95 @@
 #include "include/Terrain.h"
 
 void Terrain::createShape(std::string heightmap_path, float max_height, float vertex_spacing) {
-   std::shared_ptr<BitMap> heightmap = std::make_shared<BitMap>();
-   heightmap->load(heightmap_path);
+   this->heightmap->load(heightmap_path);
+   this->generateVerticies(max_height, vertex_spacing);
+   this->generateFaces();
+}
+
+void Terrain::generateVerticies(float max_height, float vertex_spacing) {
+   // Erase existing verticies
+   this->verticies.clear();
+
+   // Initialize verticies vector
+   vertex initial_value = {glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)};
+   this->verticies.resize(this->heightmap->width, std::vector<vertex>(this->heightmap->height, initial_value));
+
+   // Generate verticies
+   for (int z = 0; z < this->heightmap->height; z++) {
+      for (int x = 0; x < this->heightmap->width; x++) {
+         // Get heightmap pixel
+         BitMap::pixel p = this->heightmap->getPixel(x, z);
+
+         // Normalize grayscale value to 0 to 1 range
+         float normalized = p.r / 255.0f;
+
+         // Build vertex
+         vertex v;
+         v.pos.x = x * vertex_spacing;
+         v.pos.y = normalized * max_height;
+         v.pos.z = z * vertex_spacing;
+         v.normal = glm::vec3(0.0f, 1.0f, 0.0f);
+
+         this->verticies[x][z] = v;
+      }
+   }
+}
+
+void Terrain::generateFaces() {
+   // Erase existing faces
+   this->faces.clear();
+
+   // Generate faces
+   for (int z = 0; z < this->heightmap->height - 1; z++) {
+      for (int x = 0; x < this->heightmap->width - 1; x++) {
+         triangle t1;
+         t1.v1_idx = glm::ivec2(x, z);
+         t1.v2_idx = glm::ivec2(x, z + 1);
+         t1.v3_idx = glm::ivec2(x + 1, z + 1);
+
+         triangle t2;
+         t2.v1_idx = glm::ivec2(x, z);
+         t2.v2_idx = glm::ivec2(x + 1, z + 1);
+         t2.v3_idx = glm::ivec2(x + 1, z);
+
+         this->faces.push_back(t1);
+         this->faces.push_back(t2);
+      }
+   }
+}
+
+void Terrain::computeNormals() {
+   // Erase existing vertex normals
+   for (int z = 0; z < this->heightmap->height; z++) {
+      for (int x = 0; x < this->heightmap->width; x++) {
+         this->verticies[x][z].normal = glm::vec3(0.0f, 0.0f, 0.0f);
+      }
+   }
+
+   // Calculate surface normals and nonnormalized vertex normals
+   for (int i = 0; i < this->faces.size(); i++) {
+      triangle t = this->faces[i];
+      vertex *v1 = &verticies[t.v1_idx.x][t.v1_idx.y];
+      vertex *v2 = &verticies[t.v2_idx.x][t.v2_idx.y];
+      vertex *v3 = &verticies[t.v3_idx.x][t.v3_idx.y];
+
+      // Calculate surface normal
+      glm::vec3 edge1 = v2->pos - v1->pos;
+      glm::vec3 edge2 = v3->pos - v1->pos;
+
+      glm::vec3 normal = glm::normalize(glm::cross(edge1, edge2));
+      this->faces[i].normal = normal;
+
+      // Add surface normal to vertex normal
+      v1->normal += normal;
+      v2->normal += normal;
+      v3->normal += normal;
+   }
+
+   // Normalize vertex normals
+   for (int z = 0; z < this->heightmap->height; z++) {
+      for (int x = 0; x < this->heightmap->width; x++) {
+         this->verticies[x][z].normal = glm::normalize(this->verticies[x][z].normal);
+      }
+   }
 }
--- src/voyager-utils/include/Terrain.h
+++ src/voyager-utils/include/Terrain.h
@@ -5,11 +5,40 @@
 #include "Shape.h"
 #include "BitMap.h"
 
+#include <glm/glm/gtc/type_ptr.hpp>
+
 class Terrain : public Shape {
 
 public:
    
    void createShape(std::string heightmap_path, float max_height, float vertex_spacing);
+
+private:
+
+   struct vertex {
+      glm::vec3 pos;
+      glm::vec3 normal;
+   };
+
+   struct triangle {
+      glm::ivec2 v1_idx; // Index into verticies 2D vector
+      glm::ivec2 v2_idx;
+      glm::ivec2 v3_idx;
+      glm::vec3 normal;
+   };
+
+   std::shared_ptr<BitMap> heightmap = std::make_shared<BitMap>();
+   std::vector<std::vector<vertex>> verticies;
+   std::vector<triangle> faces;
+
+   void generateVerticies(float max_height, float vertex_spacing);
+   void generateFaces();
+   void computeNormals();
+
+   void buildElementBuffer();
+   void buildPositionBuffer();
+   void buildNormalBuffer();
+   void buildTextureBuffer();
 
 };
 
